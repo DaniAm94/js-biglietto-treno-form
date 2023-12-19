50f6522a95dd652cc9ac259f1bed43a7cbd3a62f
DaniAm94 <dani.amico94@gmail.com>
2023-12-19 15:37:49 +0100

    completed first milestone

--- js/script.js
+++ js/script.js
@@ -10,12 +10,18 @@ let fullPrice = 0;
 
 button.addEventListener('click', function () {
     const distance = parseInt(inputKm.value);
+    console.log(distance);
     const age = parseInt(inputAge.value);
+    console.log(age);
     inputKm.value = '';
     inputAge.value = '';
     fullPrice = 0.21 * distance;
+    console.log(fullPrice);
     result.classList.remove('d-none');
-    const finalPrice = fullPrice;
-    result.innerHTML = `Il prezzo del biglietto è <strong>${finalPrice}€</strong>`;
+    let finalPrice = fullPrice;
+
+    if (age < 18) finalPrice *= 0.8;
+    else if (age > 65) finalPrice *= 0.6;
+    result.innerHTML = `Il prezzo del biglietto è <strong>${finalPrice.toFixed(2)}€</strong>`;
     inputKm.focus();
 });
