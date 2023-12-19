46641c87c7ea7a8b3f845cc92a6219d0159ff9d6
DaniAm94 <dani.amico94@gmail.com>
2023-12-19 15:30:35 +0100

    added minimal js istructions to calculate ticket's full price

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('JS OK!');
+//Recupero gli elementi HTML
+
+const inputKm = document.getElementById('inputKm');
+const inputAge = document.getElementById('inputAge');
+const button = document.getElementById('button');
+const result = document.getElementById('result');
+
+let fullPrice = 0;
+
+
+button.addEventListener('click', function () {
+    const distance = parseInt(inputKm.value);
+    const age = parseInt(inputAge.value);
+    inputKm.value = '';
+    inputAge.value = '';
+    fullPrice = 0.21 * distance;
+    result.classList.remove('d-none');
+    const finalPrice = fullPrice;
+    result.innerHTML = `Il prezzo del biglietto è <strong>${finalPrice}€</strong>`;
+    inputKm.focus();
+});
